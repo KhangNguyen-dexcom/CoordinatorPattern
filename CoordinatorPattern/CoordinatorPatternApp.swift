66149e0d5d70c59fccac27d685129e834fd1f3cd
KhangNguyen-dexcom <tran.nguyen3@dexcom.com>
2022-07-31 19:42:37 -0500

    Initial Commit

---
+++ CoordinatorPattern/CoordinatorPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorPatternApp.swift
+//  CoordinatorPattern
+//
+//  Created by Tran Nguyen on 7/31/22.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
